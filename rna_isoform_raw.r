c24eeaff1d105a995b68169bbbfa52e524e5b488
mschubert <mschu.dev@gmail.com>
2019-09-10 00:20:38 +0200

    isoform, old fixes

--- rna_isoform_raw.r
+++ rna_isoform_raw.r
@@ -4,8 +4,12 @@ PROJECT = commandArgs(TRUE)[1]
 OUTFILE = commandArgs(TRUE)[2]
 
 query = GDCquery(project = PROJECT,
-                 data.category = "Transcriptome Profiling",
-                 data.type = "Isoform Expression Quantification")
+                 data.category = "Gene expression",
+                 data.type = "Isoform expression quantification",
+#                 experimental.strategy = "RNA-Seq",
+                 legacy = TRUE)
+
+#TODO: map hg19 to GRCh38
 
 GDCdownload(query)
 GDCprepare(query, save=TRUE, save.filename=OUTFILE, remove.files.prepared=TRUE)
