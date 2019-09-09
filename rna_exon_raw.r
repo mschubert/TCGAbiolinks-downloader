7d6ffb237b907303ede07a4564f8e6103b76662d
mschubert <mschu.dev@gmail.com>
2019-09-09 21:15:28 +0200

    filter for RNAseq v2

--- rna_exon_raw.r
+++ rna_exon_raw.r
@@ -1,4 +1,5 @@
 library(TCGAbiolinks)
+library(dplyr)
 
 PROJECT = commandArgs(TRUE)[1]
 OUTFILE = commandArgs(TRUE)[2]
@@ -7,6 +8,9 @@ query = GDCquery(project = PROJECT,
                  data.category = "Gene expression",
                  data.type = "Exon quantification",
                  legacy = TRUE)
+
+# only use RNAseq v2, otherwise error with duplicated samples
+query$results[[1]] = query$results[[1]] %>% filter(sapply(tags, function(x) x[2]) == "v2")
 
 GDCdownload(query)
 GDCprepare(query, save=TRUE, save.filename=OUTFILE, remove.files.prepared=TRUE)
