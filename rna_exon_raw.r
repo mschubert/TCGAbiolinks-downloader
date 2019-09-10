bd98cfda6fefd9803b3bace2c55b4e22b22bad8c
mschubert <mschu.dev@gmail.com>
2019-09-10 20:01:58 +0200

    explicit non-total RNA

--- rna_exon_raw.r
+++ rna_exon_raw.r
@@ -7,10 +7,14 @@ OUTFILE = commandArgs(TRUE)[2]
 query = GDCquery(project = PROJECT,
                  data.category = "Gene expression",
                  data.type = "Exon quantification",
+                 experimental.strategy = "RNA-Seq",
                  legacy = TRUE)
 
 # only use RNAseq v2, otherwise error with duplicated samples
 query$results[[1]] = query$results[[1]] %>% filter(sapply(tags, function(x) x[2]) == "v2")
+
+## also discard the ones where there are still duplicates left
+#query$results[[1]] = query$results[[1]][!duplicated(query$results[[1]]),]
 
 GDCdownload(query)
 GDCprepare(query, save=TRUE, save.filename=OUTFILE, remove.files.prepared=TRUE)
