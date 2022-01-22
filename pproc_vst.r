1ba79c42deb27d8f436f088fccedf1d06bbec22b
mschubertv <mschu.dev@gmail.com>
2022-01-22 16:49:12 +0000

    fix vst

--- pproc_vst.r
+++ pproc_vst.r
@@ -5,8 +5,8 @@ INFILE = commandArgs(TRUE)[1]
 OUTFILE = commandArgs(TRUE)[2]
 
 data = readRDS(INFILE)
-mat = assay(data)
-dset = DESeqDataSetFromMatrix(mat, colData=data.frame(id=colnames(mat)), design=~1)
-assay(data) = vst(dset)
 
-saveRDS(data, file=OUTFILE)
+eset = DESeqDataSet(data, design=~1)
+vst = varianceStabilizingTransformation(eset)
+
+saveRDS(vst, file=OUTFILE)
